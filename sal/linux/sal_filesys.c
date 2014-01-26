2607b146dff593e9ddafcb2085e27b0caf096b4e
Nebuleon Fumika <nebuleon@porygon>
2014-01-26 19:30:43 +0000

    sal/linux/sal_filesys.c: Use uint32_t, not a compiler-dependent ulong type.

--- sal/linux/sal_filesys.c
+++ sal/linux/sal_filesys.c
@@ -113,11 +113,7 @@ s32 sal_DirectoryGet(const char *path, struct SAL_DIRECTORY_ENTRY *dir,
 	s32 fileCount=0;
 	DIR *d;
 	struct dirent *de;
-	#ifdef __APPLE__
-	u_long entriesRead=0;
-	#else
-	ulong entriesRead=0;
-	#endif
+	uint32_t entriesRead=0;
 	
 	char fullFilename[256];
 	s32 endIndex=startIndex+count;
